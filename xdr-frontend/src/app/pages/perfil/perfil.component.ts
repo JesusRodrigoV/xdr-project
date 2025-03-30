b6d302991b7f3100b1f06d71e4a7e5f7b04c0f20
JesusRodrigoV <jaicelvelasco105@gmail.com>
2025-03-29 23:16:37 -0400

    Creacion frontend

---
+++ xdr-frontend/src/app/pages/perfil/perfil.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil',
+  imports: [],
+  templateUrl: './perfil.component.html',
+  styleUrl: './perfil.component.scss',
+})
+export default class PerfilComponent {}
